8cc92f2b0c426b34ed0503b1b4b1b82eb24edfff
Idel Gataullin <idel28102001@mail.ru>
2023-05-05 22:37:16 +0300

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.scss';
-
 import { Provider } from 'react-redux';
+import React from 'react';
+import './index.scss';
 
 import App from './components/App';
 import { store } from './store';
